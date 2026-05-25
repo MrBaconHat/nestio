ec678e89c9d0583ee8e666eaf2109d3b5c7f6d6c
Mr_Hat <109857632+MrBaconHat@users.noreply.github.com>
2026-05-25 16:37:33 +0000

    removed unnecessary files

--- test.py
+++
@@ -1,9 +0,0 @@
-from nestio import Json
-import asyncio 
-
-async def main():
-    file = Json("test.json")
-    await file.set("test.hi", {"yay": "hello"})
-    print(await file.get("test.hi"))
-
-asyncio.run(main())
