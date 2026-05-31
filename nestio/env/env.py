1737af735556b713861eb296762385ac7132653a
Mr_Hat <109857632+MrBaconHat@users.noreply.github.com>
2026-05-31 12:13:44 +0000

    Added support for environment variable
    can be accessed via "from nestio.env import Env"
    
    env variables cannot be wrote by the program, it has functions like get, get_int, get_float, get_list and require function, throws exception if its not found.

---
+++ nestio/env/env.py
@@ -0,0 +1,45 @@
+try:
+    from dotenv import load_dotenv
+except ImportError:
+    raise ImportError("Please install python-dotenv to use this module")
+
+load_dotenv()
+
+from pathlib import Path
+import os
+
+
+class Env:
+    # Set the default.env path(its usually in the root of the project)
+    def __init__(self, path: str = ".env"):
+        self.path = Path(path)
+
+    def __getitem__(self, key):
+        return os.getenv(key)
+
+    def require(self, key):
+        value = os.getenv(key)
+        if not value:
+            raise KeyError(f"Missing Environment variable: {key}")
+
+        return value
+
+    def get(self, key, default=None):
+        return os.getenv(key, default)
+
+    def get_int(self, key):
+        return int(os.getenv(key))
+
+    def get_float(self, key):
+        return float(os.getenv(key))
+
+    def get_bool(self, key):
+        return os.getenv(key).lower() in ("true", "1", "t", "y", "yes")
+
+    def get_list(self, key, sep=","):
+        value = os.getenv(key)
+
+        if not value:
+            return []
+
+        return [v.strip() for v in value.split(sep)]
